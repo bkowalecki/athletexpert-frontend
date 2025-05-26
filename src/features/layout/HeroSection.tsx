be23061aeeb12502030debc74b383a79894e3703
Brandon Kowalecki <bkowalecki@gmail.com>
2025-05-26 15:48:35 -0400

    performance improvements 4

--- src/features/layout/HeroSection.tsx
+++ src/features/layout/HeroSection.tsx
@@ -69,7 +69,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ openQuiz }) => {
         className="hero-video"
       >
         <source src="/video/athletexpertheadervideo.webm" type="video/webm" />
-        <source src="/video/hero-video-2.mp4" type="video/mp4" />
+        <source src="/video/hero-video-opt.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
